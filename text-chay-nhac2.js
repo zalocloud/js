6c403e141603b9bb260547d4b95a9cc5256ddd33
zalocloud <165171742+zalocloud@users.noreply.github.com>
2024-10-26 14:49:05 +0700

    Add files via upload

---
+++ text-chay-nhac2.js
@@ -0,0 +1,19 @@
+  document.addEventListener("DOMContentLoaded", function() {
+      const scrollingText = document.getElementById("scrollingText");
+      const part2 = document.querySelector(".part2");
+
+      function checkPosition() {
+        const textRect = scrollingText.getBoundingClientRect();
+        const part3Rect = document.querySelector('.part3').getBoundingClientRect();
+
+        if (textRect.left <= part3Rect.left+20) {
+          scrollingText.style.animationPlayState = "paused";
+          setTimeout(() => {
+            scrollingText.style.animationPlayState = "running";
+            part2.style.visibility = "hidden";
+          }, 1500);
+        }
+      }
+
+      setInterval(checkPosition, 100);
+    });
